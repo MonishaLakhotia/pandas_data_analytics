3a12cb47b6ffe54cfe675be5fbf770f3cd8a83f7
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-22 09:04:40 -0600

    clean up

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
+from setuptools import setup, find_packages
 # -*- coding: utf-8 -*-
 
 # from distutils.core import setup
 
-from setuptools import setup, find_packages
 
 try:
     long_description = open("README.rst").read()
@@ -10,14 +10,14 @@ except IOError:
     long_description = ""
 
 setup(
-    name="python01",
+    name="pandas_data_analytics",
     version="0.1.0",
     description="A pip package",
     license="MIT",
     author="manyu",
-    packages=find_packages(include=['python01', 'python01.*']),
+    packages=find_packages(
+        include=['pandas_data_analytics', 'pandas_data_analytics.*']),
     install_requires=[
-        "tpot",
         "sklearn"
     ],
     long_description=long_description,
