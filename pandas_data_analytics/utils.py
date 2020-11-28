102659e0e750e82f625bbf7c2c22ba817d74d4d3
Manyu <LakeMonYew@gmail.com>
2020-11-27 19:11:32 -0600

    Adds pycon df notes

--- pandas_data_analytics/utils.py
+++ pandas_data_analytics/utils.py
@@ -12,6 +12,28 @@ import functools as ft
 # number of unique entries for the itemDescription column
 # len(df["itemDescription"].unique())
 
+# get 5 random samples
+# df[['event', 'date']].sample(5)
+
+# convert date like field to a date type. 's' is for unix time stamp to date
+# pd.to_datetime(df['date_like_field'], unit='s')
+
+# get year of date and count the freq of each year
+# x = df['date'].dt.year.value_counts()
+# x.sort_index() sorts the x values?
+
+# group by stuff -- NOTE: count is the number of rows per group
+# df.groupby('event')['views'].agg['count', 'mean', 'sum'].sort_values('sum')
+
+# a safer eval to parse a string 
+# import ast
+# type(ast.literal_eval('[1,2,3]')) # outputs: list
+
+# filtering df using a series
+# occ_counts = df['occ'].value_counts()
+# top_occs = occ_counts[occ_counts >= 5].index
+# filtered_df = df[df['occ'].isin(top_occs)]
+
 
 def l(df): return [df.head(), df.dtypes, df.shape, df.columns, df.index]
 
