71ad5a8bc32ee29112378f7bfdac13210637561a
Manyu Lakhotia <lakemonyew@gmail.com>
2020-12-05 15:27:16 -0600

    Reads pdf page at a time rather than the whole file

--- pandas_data_analytics/text_parser/main.py
+++ pandas_data_analytics/text_parser/main.py
@@ -3,12 +3,22 @@ from py_linq import Enumerable
 from pandas_data_analytics.text_parser.parser import Parser
 
 
-def from_pdf(pdf_loc, settings):
+def from_pdf_bulk_read(pdf_loc, settings):
     parser = Parser(parser_settings)
     with pdfplumber.open(pdf_loc) as pdf:
         all_txt = Enumerable(pdf.pages).aggregate(
             lambda acc, page: acc+page.extract_text(), "")
         result = parser.parse(all_txt)
+        return result
+
+
+def from_pdf(pdf_loc, settings):
+    parser = Parser(parser_settings)
+    with pdfplumber.open(pdf_loc) as pdf:
+        result = Enumerable(pdf.pages)\
+            .select(lambda page: page.extract_text())\
+            .select(lambda page_txt: parser.parse(page_txt))\
+            .aggregate(merge_dicts)
         return result
 
 
--- pandas_data_analytics/text_parser/run.py
+++ pandas_data_analytics/text_parser/run.py
@@ -15,8 +15,22 @@ pdf_loc = config['file_locations']['data']
 parser_settings = toml.load(os.path.join(this_dir, 'pdf_config.toml'))
 parser = Parser(parser_settings)
 
+
+def merge_dicts(d1, d2):
+    # dicts must have the exact same keys
+    # if values from d1 are None, then use d2's value
+    d3 = {}
+    for k, v in d1.items():
+        if v is None or v == '':
+            d3[k] = d2[k]
+        else:
+            d3[k] = v
+    return d3
+
+
 with pdfplumber.open(pdf_loc) as pdf:
-    all_txt = Enumerable(pdf.pages).aggregate(
-        lambda acc, page: acc+page.extract_text(), "")
-    result = parser.parse(all_txt)
+    result = Enumerable(pdf.pages)\
+        .select(lambda page: page.extract_text())\
+        .select(parser.parse)\
+        .aggregate(merge_dicts)
     print(result)
