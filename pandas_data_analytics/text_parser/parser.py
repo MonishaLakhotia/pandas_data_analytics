fd141da70a2bfe3cd1d4a67fe33f5095892e8865
Manyu <LakeMonYew@gmail.com>
2020-12-04 19:10:36 -0600

    Works on text parser

--- pandas_data_analytics/text_parser/parser.py
+++ pandas_data_analytics/text_parser/parser.py
@@ -1,17 +1,40 @@
 import re
 from py_linq import Enumerable
 
+def str_flags_to_regex_flags(str_flags):
+    flag_map = {
+        "i": re.IGNORECASE,
+        "x": re.VERBOSE,
+        "m": re.MULTILINE,
+        "s": re.DOTALL,
+        "u": re.UNICODE,
+        "l": re.LOCALE,
+        "d": re.DEBUG,
+        "a": re.ASCII,
+        "t": re.TEMPLATE
+    }
+    def r(acc, ele):
+        acc = acc | ele
+        return acc
+    return Enumerable(str_flags)\
+      .select(lambda f: flag_map[f])\
+      .aggregate(r)
+
 class Parser(object):
 
   def __init__(self, settings):
     self.settings = settings
     
   def get_match_string(self, match, ordered_groups, group_join_symbol):
+    m = re.sub('.*?Patient #: (\d+) Capture:.*', '\\1', '  Patient #: 234234 Capture:23 ')
     return group_join_symbol.join(Enumerable(ordered_groups).select(lambda g: match.groups[g].value.trim()))
 
   def parse(self, txt):
     result_dict = {}
     s = self.settings
-    for name,v in s.items():
-      result_dict[name] = self.get_match_string(re.match(v.patten, txt, flags=v.flags))
+    for v in s['fields']:
+      list_of_replace_strings = Enumerable(v.get('ordered_groups',[1]))\
+        .select(lambda ele: '\\' + str(ele)).to_list()
+      result_dict[v['name']] = re.sub(v['pattern'], v.get('group_join_symbol','').join(list_of_replace_strings)\
+          ,txt, flags=str_flags_to_regex_flags(v['flags']))
     return result_dict
--- pandas_data_analytics/text_parser/run.py
+++ pandas_data_analytics/text_parser/run.py
@@ -3,6 +3,8 @@ from py_linq import Enumerable
 import toml
 import os
 import pandas_data_analytics.utils as u
+from pandas_data_analytics.text_parser.parser import Parser
+import re
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 config = toml.load(os.path.join(this_dir, 'config.toml'))
@@ -10,5 +12,15 @@ config = toml.load(os.path.join(this_dir, 'config.toml'))
 u.set_full_paths(config, this_dir)
 pdf_loc = config['file_locations']['data']
 
+parser_settings = toml.load(os.path.join(this_dir, 'pdf_config.toml'))
+parser = Parser(parser_settings)
+
+m = re.sub('.*?(\w+).*', '\\1', '  words ')
+m = re.sub('.*?Patient #: (\d+) Capture:.*', '\\1', '  Patient #: 234234 Capture:23 ')
+# m = re.search('.*?(\w+).*?', '\\1', '  words ')
+# m2 = ', '.join(re.findall('words (\w+)', '  words  pa'))
+
 with pdfplumber.open(pdf_loc) as pdf:
     all_txt = Enumerable(pdf.pages).aggregate(lambda acc, page: acc+page.extract_text(), "")
+    result = parser.parse(all_txt)
+    print(result['milk'])
