3b16549f16dae994a6a940ebc02ee3f62dadf188
Manyu <LakeMonYew@gmail.com>
2020-12-17 16:33:58 -0600

    adds config and el pollo stub

---
+++ pandas_data_analytics/fast_food/el_pollo_loco.py
@@ -0,0 +1,43 @@
+import pandas_data_analytics.utils as u
+import re
+import toml
+from pandas_data_analytics import *
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import functools as ft
+from py_linq import Enumerable
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+config = toml.load(os.path.join(this_dir, 'config.toml'))
+u.set_full_paths(config, this_dir)
+csv_loc = config['file_locations']['raw_el_pollo_loco']
+
+df: pd.DataFrame = pd.read_csv(csv_loc)
+df.columns = df.columns.str.lower()
+df.drop(['web-scraper-order', 'web-scraper-start-url'], inplace=True, axis=1)
+
+pd.set_option('display.max_rows', df.shape[0]+1)
+pd.set_option('display.max_columns', df.shape[1]+1)
+
+ps = (lambda pdf: Enumerable([
+  lambda: pdf.sample(5),
+  lambda: pdf.columns,
+  # lambda: pdf.dtypes,
+  # lambda: pdf[['food', 'cal_per_gram', 'category']].groupby('category').food.agg(list),
+  # lambda: pdf.category.value_counts(),
+]))(df)
+
+u.foreach(lambda f: print(f()),ps)
+
+# df.to_csv(config['file_locations']['clean_el_pollo_loco'], index=False)
+
+# Apply the default theme
+sns.set_theme()
+
+# aplot = sns.boxplot(x='category', y='cal_per_gram', data=df)
+# General plot stuff
+# aplot.set_xticklabels(aplot.get_xticklabels(), rotation=30)
+# plt.show()
