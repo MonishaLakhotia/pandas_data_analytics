6c3259545f4a8fd698f589712a0b4d63a56a48fb
Manyu <LakeMonYew@gmail.com>
2020-12-18 16:00:33 -0600

    Adds company file

--- pandas_data_analytics/fast_food/clean_scripts/menshealth_40_worst.py
+++ pandas_data_analytics/fast_food/clean_scripts/menshealth_40_worst.py
@@ -47,6 +47,39 @@ def get_sugar(s):
   return m if not pd.isna(m)\
     else get_thing(s, 'sugar[\sa-zA-Z]\((\d+[\d,\.]+)[\sa-zA-Z]*?\)')
 
+def get_company(s):
+  return 'Chilis' if re.search('Chili\'s', s, re.I)\
+    else 'Sonic' if re.search('Sonic', s, re.I)\
+    else 'AMC Theaters' if re.search('AMC Theaters', s, re.I)\
+    else 'Wendys' if re.search('Wendy\'s', s, re.I)\
+    else 'Panera Bread' if re.search('Panera Bread', s, re.I)\
+    else 'Romanos' if re.search('Romano’s', s, re.I)\
+    else 'Checkers' if re.search('Checkers\'', s, re.I)\
+    else 'Jimmy Johns' if re.search('Jimmy John\'s', s, re.I)\
+    else 'McDonalds' if re.search('McDonald\'s', s, re.I)\
+    else 'Arbys' if re.search('Arby’s', s, re.I)\
+    else 'Outback Steakhouse' if re.search('Outback Steakhouse', s, re.I)\
+    else 'Applebees' if re.search('Applebee\'s', s, re.I)\
+    else 'KFC' if re.search('KFC', s, re.I)\
+    else 'Cheesecake Factory' if re.search('Cheesecake Factory', s, re.I)\
+    else 'Carls Jrs' if re.search('Carl\'s Jr\'s', s, re.I)\
+    else 'Yard House' if re.search('Yard House', s, re.I)\
+    else 'BJ\'s Restaurant & Brewhouse' if re.search('BJ\'s Restaurant & Brewhouse', s, re.I)\
+    else 'El Pollo Loco' if re.search('El Pollo Loco', s, re.I)\
+    else 'In N Out' if re.search('In N\' Out', s, re.I)\
+    else 'Dunkin Donuts' if re.search('Dunkin Donut\'s', s, re.I)\
+    else 'Corner Bakery' if re.search('Corner Bakery', s, re.I)\
+    else 'IHOPs' if re.search('IHOP\'s', s, re.I)\
+    else 'Olive Garden' if re.search('Olive Garden', s, re.I)\
+    else 'Uno Pizzeria & Grill' if re.search('Uno Pizzeria & Grill', s, re.I)\
+    else 'Taco Bell' if re.search('Taco Bell', s, re.I)\
+    else 'Jack In The Box' if re.search('Jack In The Box', s, re.I)\
+    else 'Schlotzskys' if re.search('Schlotzsky’s', s, re.I)\
+    else 'Subway' if re.search('Subway', s, re.I)\
+    else 'Burger King' if re.search('Burger King', s, re.I)\
+    else 'Shake Shack' if re.search('Shake Shack', s, re.I)\
+    else np.nan
+
 for (label, fn) in [\
   ('calories', get_calories),\
   ('sodium_in_milligrams', get_sodium),\
@@ -57,6 +90,7 @@ for (label, fn) in [\
     ]:
   df[label] = df.descr.apply(fn)#.str.replace(',', '')
 
+df['company'] = df.name.apply(get_company)
 
 df.drop(['web-scraper-order', 'web-scraper-start-url'], inplace=True, axis=1)
 # total_rows
@@ -64,13 +98,14 @@ df.drop(['web-scraper-order', 'web-scraper-start-url'], inplace=True, axis=1)
 pd.set_option('display.max_rows', df.shape[0]+1)
 pd.set_option('display.max_columns', df.shape[1]+1)
 
-ps = (lambda pdf: Enumerable([
+pdf = df
+ps = Enumerable([
   # lambda: pdf[['name']],
   lambda: pdf.columns,
   lambda: pdf,
   # lambda: pdf[['food', 'cal_per_gram', 'category']].groupby('category').food.agg(list),
   # lambda: pdf.category.value_counts(),
-]))(df)
+])
 u.foreach(lambda f: print(f()),ps)
 
 # df.to_csv(config['file_locations']['clean_mens_health_40_worst'], index=False)
