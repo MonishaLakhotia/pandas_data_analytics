7042b9805caf7f2b0a0c2fde0a201d741ec5d234
Manyu <LakeMonYew@gmail.com>
2020-12-18 09:06:21 -0600

    cleans el pollo loco data

--- pandas_data_analytics/fast_food/clean_scripts/el_pollo_loco.py
+++ pandas_data_analytics/fast_food/clean_scripts/el_pollo_loco.py
@@ -17,10 +17,17 @@ csv_loc = config['file_locations']['raw_el_pollo_loco']
 
 df: pd.DataFrame = pd.read_csv(csv_loc)
 df.columns = df.columns.str.lower()
+
+for label in ['fat', 'protein', 'carbs']:
+  df[label] = df[label].str.split('g', expand=True)[0]
+
+df['calories'] = df.calories.astype('Int64')
 df['category'] = df.main_food_link
-df.drop(['web-scraper-order', 'web-scraper-start-url', 'main_food_link', 'main_food_link'], inplace=True, axis=1)
+df.drop(['web-scraper-order', 'web-scraper-start-url', 'main_food_link', 'main_food_link-href'], inplace=True, axis=1)
 df['company'] = 'El Pollo Loco'
-df.dropna(inplace=True, axis=0)
+df.dropna(inplace=True, axis=0, subset=['calories'])
+df['serving_size_unit'] = df.serving_size.str.split(' ', expand=True)[1]
+df['serving_size'] = df.serving_size.str.split(' ', expand=True)[0]
 
 pd.set_option('display.max_rows', df.shape[0]+1)
 pd.set_option('display.max_columns', df.shape[1]+1)
@@ -28,18 +35,11 @@ pd.set_option('display.max_columns', df.shape[1]+1)
 ps = (lambda pdf: Enumerable([
   # lambda: pdf[['name']],
   lambda: pdf.columns,
-  # lambda: pdf.dtypes,
+  lambda: pdf.serving_size_unit.value_counts(dropna=False),
+  lambda: pdf.sample(10),
   # lambda: pdf[['food', 'cal_per_gram', 'category']].groupby('category').food.agg(list),
   # lambda: pdf.category.value_counts(),
 ]))(df)
 u.foreach(lambda f: print(f()),ps)
 
 # df.to_csv(config['file_locations']['clean_el_pollo_loco'], index=False)
-
-# Apply the default theme
-sns.set_theme()
-
-# aplot = sns.boxplot(x='category', y='cal_per_gram', data=df)
-# General plot stuff
-# aplot.set_xticklabels(aplot.get_xticklabels(), rotation=30)
-# plt.show()
