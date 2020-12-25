a5b1c2317d379664b8ec8eb3fb05b99961ac93be
Manyu <LakeMonYew@gmail.com>
2020-12-25 11:27:23 -0600

    Adds notes on shift

--- pandas_data_analytics/pandas_notes.py
+++ pandas_data_analytics/pandas_notes.py
@@ -173,6 +173,13 @@
 # counts all rows with a unique index value
 # len(df.index.value_counts()),
 
+# subtract prev value from current value and store in new column
+# dfe = pd.DataFrame({"Col1": [10, 20, 15, 30, 45],
+#                    "Col2": [13, 23, 18, 33, 48],
+#                    "Col3": [17, 27, 22, 37, 52]},
+#                   index=pd.date_range("2020-01-01", "2020-01-05"))
+# dfe['Col4'] = dfe.Col3.shift(1) - dfe.Col3
+
 
 #### QT CONSOLE #####
 # allows more space for column contents
