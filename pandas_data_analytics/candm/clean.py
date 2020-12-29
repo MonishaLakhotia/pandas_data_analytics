d75d857cd1b76c727cc07babc5f59c04d26bf665
Manyu <LakeMonYew@gmail.com>
2020-12-29 08:18:52 -0600

    Cleans imports on candm clean

--- pandas_data_analytics/candm/clean.py
+++ pandas_data_analytics/candm/clean.py
@@ -1,15 +1,14 @@
 import pandas_data_analytics.utils as u
 import toml
+import re
 import os
 import pandas as pd
-import pandas_data_analytics.pdpipe_example.pipe_utils as pu
-import pandas_data_analytics.pdpipe_example.clean as c
-import functools as ft
+import seaborn as sns
 from py_linq import Enumerable
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 config = toml.load(os.path.join(this_dir, 'config.toml'))
-pu.set_full_paths(config, this_dir)
+u.set_full_paths(config, this_dir)
 csv_loc = config['file_locations']['raw']
 df = pd.read_csv(csv_loc)
 
@@ -24,5 +23,5 @@ def brand_mapper(row):
         row['brand'] = matching_brand
     return row
 
-df = df.apply(brand_mapper, axis=1)
-df.to_csv(config['file_locations']['clean'])
+df: pd.DataFrame = df.apply(brand_mapper, axis=1)
+df.to_csv(config['file_locations']['clean'], index=False)
