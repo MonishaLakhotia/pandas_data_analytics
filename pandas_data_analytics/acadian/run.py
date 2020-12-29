a185fbdccb5125e5ba0c2749bbb6e7d812259245
Manyu <LakeMonYew@gmail.com>
2020-12-29 08:14:56 -0600

    Cleans imports for acadian

--- pandas_data_analytics/acadian/run.py
+++ pandas_data_analytics/acadian/run.py
@@ -1,25 +1,17 @@
 import pandas_data_analytics.utils as u
 import toml
 import re
-from pandas_data_analytics import *
 import os
-import pdpipe as pdp
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import stats
 import seaborn as sns
 import numpy as np
-import joblib
-import pandas_data_analytics.pdpipe_example.pipe_utils as pu
-import pandas_data_analytics.pdpipe_example.clean as c
-import functools as ft
+from py_linq import Enumerable
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 config = toml.load(os.path.join(this_dir, 'config.toml'))
-
-pu.set_full_paths(config, this_dir)
+u.set_full_paths(config, this_dir)
 csv_loc = config['file_locations']['training_data']
-
 df = pd.read_csv(csv_loc)
 
 def r(s):
