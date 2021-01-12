6c7a4f282f61403cf491d1e4eec656c2f86d6989
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-12 11:13:48 -0600

    Adds monthly summaries

--- pandas_data_analytics/body_comp/run.py
+++ pandas_data_analytics/body_comp/run.py
@@ -25,16 +25,21 @@ df.date = pd.to_datetime(df.date)
 pd.set_option('display.max_rows', df.shape[0]+1)
 pd.set_option('display.max_columns', 175)
 
-df = df[(df['date']>pd.Timestamp(2020,11,24)) & (df['date']<pd.Timestamp(2021,1,23))]
+date_bins = pd.interval_range\
+  (start=pd.Timestamp('2020-07-01'),\
+  periods=7, freq='MS')
+df['date_range'] = pd.cut(df.date, date_bins)
+recentdf = df#[(df['date']>pd.Timestamp(2020,11,24)) & (df['date']<pd.Timestamp(2021,1,23))]
 
-diffdf = df.iloc[-1] - df.iloc[0]
-pdf = df
+# diffdf = recentdf.iloc[-1] - recentdf.iloc[0]
+pdf: pd.DataFrame = recentdf
 ps = Enumerable([
   lambda: pdf.columns,
-  lambda: pdf.dtypes,
-  lambda: diffdf,
-  lambda: pdf.describe(include='all', datetime_is_numeric=True),
-  lambda: pdf,
+  # lambda: date_bins,
+  # lambda: pdf.dtypes,
+  # lambda: pdf.describe(include='all', datetime_is_numeric=True),
+  # lambda: pdf.sample(5),
+  # lambda: monthly_summaries_df,
   # lambda: pdf.sort_values(['name', 'generation', 'moves_learnt_by_level_up_lvl']).drop([], axis=1).sample(5),
   # lambda: pdf.sort_values(['name', 'generation', 'moves_learnt_by_level_up_lvl']).sample(5),
   # lambda: pdf[movesdf.duplicated()].sort_values(['name', 'generation'])
@@ -42,13 +47,16 @@ ps = Enumerable([
 ])
 u.foreach(lambda f: print(f()),ps)
 
-# df.to_csv(config['file_locations']['clean_data'])
-
+# write out monthly summaries of stats
+monthly_summaries_df: pd.DataFrame = pdf.groupby(by='date_range').agg(['mean', 'std', 'min', 'max']).unstack().reset_index()
+monthly_summaries_df.columns = monthly_summaries_df.columns.astype(str)
+monthly_summaries_df.rename(columns={'level_0':'metric', 'level_1':'stat', '0': 'num'}, inplace=True)
+monthly_summaries_df.to_csv(config['file_locations']['monthly_summaries'], index=False)
 
 # Apply the default theme
 sns.set_theme()
 
-# aplot = sns.boxplot(x='food_cat', y='carbs_marco_ratio', data=df)
+# aplot = sns.lineplot(x='date', y='morning_weight', data=df)
 # General plot stuff
 # aplot.set_xticklabels(aplot.get_xticklabels(), rotation=30)
 # plt.show()
