544246749815ee0724a91e05d225a2c1202c005f
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-14 16:43:21 -0600

    Finishes first draft of pad

--- pad.sh
+++ pad.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-echo "#: $#"
-echo "@: $@"
+# Default values
+valC="X"
+valN="10"
+valM="left"
+method_choices=("left", "right")
+
 while [[ $# > 0 ]]
 do
   case "$1" in
 
-    -c|--valueC)
+    -c|--character)
             valC="$2"
             shift
             ;;
 
-    -n|--valueN)
+    -n|--number)
             valN="$2"
             shift
             ;;
 
-    --help)
+    -m|--method)
+            valM="$2"
+            shift
+            ;;
+
+    -h|--help)
             echo "Usage:"
-            echo "    --valueC \"value\""
-            echo "    --valueN \"value\""
-            echo "    --help"
+            echo "    -c|--character \"Character to pad string with. Default is: $valC\""
+            echo "    -n|--number \"Number of characters the string should be. Default is: $valN\""
+            echo "    -m|--method \"Which way to pad the string. Default is: $valM. Choices: $(printf '%s\n' ${method_choices[@]})\""
+            echo "    -h|--help"
             exit 1
             ;;
 
@@ -35,26 +45,16 @@ done
 
 # [[ -p /dev/stdin ]] && { mapfile -t; set -- "${MAPFILE[@]}"; }
 
-echo "#: $#"
-echo "@: $@"
-
-# valC="X"
-# valN="10"
-# strs=("pad" "me\n" "daddy ya ya")
-# Load the user defined parameters
-echo "C: $valC"
-echo "N: $valN"
+# TODO: validate each input for the flags
 
 iN="$(printf '%d' $valN 2>/dev/null)"
-
-echo "iN: $iN"
 
 declare -a arr
 for str in ${strs[@]}
 do
   while ((${#str} < $iN)); do 
-    # str+="$valC" # for pad right
-    str="$valC$str" # for pad left
+    [[ "$valM" == "right" ]] && { str+="$valC"; }
+    [[ "$valM" == "left" ]] && { str="$valC$str"; }
   done
   arr=( "${arr[@]}" "$str" )
 done
