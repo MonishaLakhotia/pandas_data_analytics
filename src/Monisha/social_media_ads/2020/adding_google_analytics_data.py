dd7242f977072e44919c036eff36ca689bf1df43
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-06-22 16:07:35 -0400

    Adds keywords + content google analytics matching

--- src/Monisha/social_media_ads/2020/adding_google_analytics_data.py
+++ src/Monisha/social_media_ads/2020/adding_google_analytics_data.py
@@ -1,4 +1,4 @@
-from numpy.core.numeric import NaN
+from numpy.core.numeric import NaN, outer
 import pandas as pd
 import numpy as np
 import re
@@ -13,8 +13,9 @@ cowboy_come_home = pd.read_csv('~/Desktop/Google_Analytics_2020/No_Date_Overlap/
 first_kiss_with_a_cowboy = pd.read_csv('~/Desktop/Google_Analytics_2020/No_Date_Overlap/First_Kiss_With_A_Cowboy_Analytics HBG+Imprint_ Forever Missing  Keyword - 2020 - PaidSocial 20200501-20200614-3.csv', \
   skiprows=6)
 
-#google data that has keywords but no content tag
-keywords_no_content = pd.read_csv('~/Desktop/Google_Analytics_2020/Keywords_No_Content/Actual_Keywords_NoAdContent_Analytics HBG+Imprint_ Forever Full  View 2020 20200101-20201231-1.csv', skiprows=6)
+#google data that has keywords but no content tag, keywords content and one pinterest ad
+keywords_no_content = pd.read_csv('~/Desktop/Google_Analytics_2020/Keywords_No_Content/W Paris Secret - Keywords No Content Analytics HBG+Imprint_ Forever  Full View 2020 20200101-20201231-7.csv', skiprows=6)
+keywords_content_onepin = pd.read_csv('~/Desktop/Google_Analytics_2020/Keywords_Content_1Pinterest/Keywords Content One Pin Analytics HBG+Imprint_ Forever Full View  2020 20200101-20201231-5.csv', skiprows=6)
 
 pd.set_option('display.max_columns', None)
 
@@ -109,8 +110,10 @@ no_overlap(ill_be_watching_you)
 no_overlap(first_kiss_with_a_cowboy)
 
 #FOR KEYWORDS BUT NO CONTENT
-#drop colums Date Range and Ad Content
+#drop colums Date Range and Ad Content, drop the-paris-secret where keyword is (not set)
 keywords_no_content.drop(['Date Range', 'Ad Content'], axis=1, inplace=True)
+keywords_no_content.drop(keywords_no_content.loc[(keywords_no_content.Campaign == 'the-paris-secret') & (keywords_no_content.Keyword == '(not set)')].index, axis=0, inplace=True)
+keywords_no_content.drop(keywords_no_content.loc[(keywords_no_content.Campaign == 'the-paris-secret') & (keywords_no_content.Keyword == 'bookdiscussionclub')].index, axis=0, inplace=True)
 
 #separating out Users and Clicks to Retail, dropping Segment, and renaming the Users Column for Clicks to Retail df
 keywords_no_content_ctr = keywords_no_content.loc[keywords_no_content.Segment == 'Click to Retail', :].drop(columns='Segment').rename(columns={'Users': 'Click_To_Retail'})
@@ -130,6 +133,7 @@ for value in keywords_no_content_new.Campaign:
 #creating the Audience matching column in keywords_matching
 keywords_matching['Audience'] = keywords_matching.Audience.str.split(' - ', expand=True)
 keywords_matching['Audience'] = keywords_matching.Audience.str.replace(' ', '').str.lower()
+keywords_matching['Audience'] = keywords_matching.Audience.str.replace('natashalester', 'parisorphanv1')
 
 #changing Placements/Source columsn in keywords_matching and keyword_no_content_new for merge
 keywords_matching.loc[keywords_matching.Placements.str.contains('FB'), 'Placements'] = 'Facebook'
@@ -140,17 +144,74 @@ keywords_no_content_new['Keyword'] = keywords_no_content_new.Keyword.replace('(n
 keywords_no_content_new['Users'] = keywords_no_content_new.Users.str.replace(',', '').astype(float)
 keywords_no_content_new['Click_To_Retail'] = keywords_no_content_new.Click_To_Retail.astype(float)
 
-#merginging keywords_matching with keywords_no_content_new
+#merging keywords_matching with keywords_no_content_new
 no_content_merged = pd.merge(keywords_no_content_new, keywords_matching, left_on=['Campaign', 'Keyword', 'Source'], right_on=['Book_Matching', 'Audience', 'Placements'], how='outer')
 
 #dropping columns from no_content_merged for merge to main social_ads df
+no_content_merged.dropna(subset=['Campaign'], inplace=True)
+
 no_content_merged.drop(columns=['Source', 'Keyword', 'Campaign', 'Book_Matching', 'Placements', 'Audience'], inplace=True)
 
+#merging to main social doc
 social_data = pd.merge(social_data, no_content_merged, on=['Campaign_Name'], how='outer',suffixes=('_z', '_a'))
 
 
+#FOR KEYWORDS AND CONTENT AND ONE PINTEREST AD
+#drop Date Range column
+keywords_content_onepin.drop('Date Range', axis=1, inplace=True)
 
-##MOVE THIS AFTER THE NEXT ROUND
+#separate out Users and Clicks to Retail
+users = keywords_content_onepin.loc[keywords_content_onepin.Segment == 'All Users'].drop(columns='Segment')
+clicks_to_retail = keywords_content_onepin.loc[keywords_content_onepin.Segment == 'Click to Retail'].drop(columns='Segment').rename(columns={'Users': 'Click_To_Retail'})
+
+#merge Users and Clicks to Retail
+keywords_content_onepin_new = pd.merge(users, clicks_to_retail)
+
+#convert Users\s and Clicks to Retail to float
+keywords_content_onepin_new['Users'] = keywords_content_onepin_new.Users.str.replace(',', '').astype(float)
+keywords_content_onepin_new['Click_To_Retail'] = keywords_content_onepin_new.Click_To_Retail.astype(float)
+
+#change Campaign to Book Matching Style
+keywords_content_onepin_new['Campaign'] = keywords_content_onepin_new.Campaign.str.replace('-', '').str.upper().str.replace('ALADYSGUIDE', 'ALADYSGUIDETOMISCHIEFANDMAYHEM')
+
+#breaking out the Books that correspond to the keywords_content_onepin_new df Campaigns
+keywords_matching_2 = pd.DataFrame()
+for value in keywords_content_onepin_new.Campaign:
+  keywords_matching_2 = pd.concat([keywords_matching_2, social_data.loc[value == social_data.Book_Matching, ['Campaign_Name', 'Book_Matching', 'Placements', 'Audience', 'Objective', 'Result_Type']]], axis=0).drop_duplicates()
+
+#creating the Audience matching column in keywords_matching_2
+keywords_matching_2['Audience'] = keywords_matching_2.Audience.str.split(' - ', expand=True)
+keywords_matching_2['Audience'] = keywords_matching_2.Audience.str.split('(', expand=True)
+keywords_matching_2['Audience'] = keywords_matching_2.Audience.str.split('/', expand=True)
+keywords_matching_2['Audience'] = keywords_matching_2.Audience.str.replace(' ', '').str.replace('\'', '').str.lower()
+
+#changing Ad Content to Result_Type format
+keywords_content_onepin_new['Ad Content'] = keywords_content_onepin_new['Ad Content'].str.replace('-', ' ').str.title().str.replace('Conversions', 'Conversion')
+keywords_content_onepin_new.loc[keywords_content_onepin_new.Campaign == 'THETWELVEDOGSOFCHRISTMAS', 'Ad Content'] = keywords_content_onepin_new['Ad Content'].str.replace('Impressions', 'Conversion')
+
+#changing Placements/Source columsn in keywords_matching and keyword_no_content_new for merge
+keywords_matching_2.loc[keywords_matching_2.Placements.str.contains('FB'), 'Placements'] = 'Facebook'
+keywords_matching_2.loc[keywords_matching_2.Placements.str.contains('Pin Feed'), 'Placements'] = 'Pinterest'
+keywords_matching_2.loc[keywords_matching_2.Placements.str.contains('IG'), 'Placements'] = 'Instagram'
+keywords_content_onepin_new['Source'] = keywords_content_onepin_new.Source.str.title().str.replace('Facebook-Instagram', 'Facebook')
+keywords_content_onepin_new['Keyword'] = keywords_content_onepin_new.Keyword.replace('(not set)', NaN)
+keywords_content_onepin_new['Ad Content'] = keywords_content_onepin_new['Ad Content'].replace('(Not Set)', 'Clicks')
+keywords_content_onepin_new.loc[keywords_content_onepin_new.Campaign == 'ALADYSGUIDETOMISCHIEFANDMAYHEM', 'Ad Content'] = keywords_content_onepin_new['Ad Content'].str.replace('Landing Page Views', 'Link Clicks')
+
+#merging keywords_matching_2 with keywords_content_onepin_new
+keywords_content_onepin_merged = pd.merge(keywords_content_onepin_new, keywords_matching_2, left_on=['Campaign', 'Keyword', 'Source', 'Ad Content'], right_on=['Book_Matching', 'Audience', 'Placements', 'Result_Type'], how='outer')
+
+#misc cleaning - drop Campaigns that have NaN values
+keywords_content_onepin_merged.dropna(subset=['Campaign', 'Campaign_Name'], inplace=True)
+
+#dropping columns from no_content_merged for merge to main social_ads df
+keywords_content_onepin_merged.drop(columns=['Campaign', 'Source', 'Keyword', 'Ad Content', 'Book_Matching', 'Placements', 'Audience', 'Objective', 'Result_Type'], inplace=True)
+
+#merging to social_data
+social_data = pd.merge(social_data, keywords_content_onepin_merged, on=['Campaign_Name'], how='outer')
+
+"""
+##MOVE THIS AFTER THE NEXT ROUND -- haven't added this part to the doc notes
 #adding the no_overlaps to create Users_New and Click_To_Retail_New
 cols_Users = ['Users_x', 'Users_y', 'Users_z', 'Users_a']
 social_data = social_data.assign(Users_New = social_data[cols_Users].sum(axis=1))
@@ -167,5 +228,6 @@ print(social_data.info())
 print(social_data.loc[social_data.Users_New != 0, :])
 
 
-
 #for the merge, add the _drop kind of thing instead o letting it do _x
+#END MOVE NOTES
+"""
