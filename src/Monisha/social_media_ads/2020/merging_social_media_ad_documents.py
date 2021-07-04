129debf6a2ce769f2b4578fe03d8a5feff0d6c99
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-07-04 12:36:03 -0400

    Intros configuration flow

--- src/Monisha/social_media_ads/2020/merging_social_media_ad_documents.py
+++ src/Monisha/social_media_ads/2020/merging_social_media_ad_documents.py
@@ -4,20 +4,29 @@ from numpy.core.numeric import NaN
 import pandas as pd
 import numpy as np
 import re
+import os
+import toml
+import src.utils as u
 
-ad_schedule = pd.read_csv('~/Desktop/Social_Ads_Analytics_2020/SocialAdsSchedule_2020.csv')
-facebook_instagram = pd.read_csv('~/Desktop/Social_Ads_Analytics_2020/FacebookData_2020.csv')
-pinterest = pd.read_csv('~/Desktop/Social_Ads_Analytics_2020/PinterestData_2020.csv')
+# get the directory path where this script is
+this_dir = os.path.dirname(os.path.realpath(__file__))
+# grab the config file from this directory
+config = toml.load(os.path.join(this_dir, 'config.toml'))
+# prefix partial file paths found in the config.toml with the full path
+u.set_full_paths(config, this_dir)
+
+ad_schedule = pd.read_csv(config['file_locations']['ad_schedule'])
+facebook_instagram = pd.read_csv(config['file_locations']['facebook_instagram'])
+pinterest = pd.read_csv(config['file_locations']['pinterest'])
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 #facebook_instagram drop columns, rename columns, set index to Campaign_Name, rename Result_Type
 facebook_instagram.drop(columns=['Reporting Starts', 'Reporting Ends', 'Ad Set Budget Type', 'Ends', 'Starts', 'Ad Set Budget'], inplace=True)
 facebook_instagram.rename(columns={
   'Campaign Name': 'Campaign_Name',  
   'Amount Spent (USD)': 'Spend', 
-  'Objective': 'Objective', 
-  'Reach': 'Reach', 
   'Link Clicks': 'Clicks', 
-  'Results': 'Results', 
   'Result Indicator': 'Result_Type', 
   'Cost per Results': 'Cost_Per_Result'}, inplace=True)
 facebook_instagram.set_index('Campaign_Name', inplace=True)
@@ -31,12 +40,12 @@ facebook_instagram.Result_Type.replace({
   'reach': 'Reach'}, inplace=True)
 facebook_instagram['Results'] = facebook_instagram.Results.replace(',', '')
 
+#facebook_instagram.columns = facebook_instagram.columns.str.strip().str.replace('\s+', '_', regex=True)
+#print(facebook_instagram.columns)
 #pinterest drop columns, rename columns, set index to Campaign_Name
 pinterest.drop(columns=['Campaign status', 'Campaign ID', 'Campaign creative type', 'Cost per result type', 'Campaign Budget', 'Campaign start date/time', 'Campaign end date/time'], inplace=True)
 pinterest.rename(columns={
-  'Campaign name': 'Campaign_Name',
-  'Spend': 'Spend', 
-  'Reach': 'Reach', 
+  'Campaign name': 'Campaign_Name', 
   'Paid outbound clicks': 'Clicks', 
   'Result': 'Results', 
   'Cost per result': 'Cost_Per_Result', 
@@ -53,7 +62,7 @@ ad_schedule.rename(columns={
   'Pub Day': 'Release_Date',
   '@': 'Daily_Budget'}, inplace=True)
 ad_schedule.set_index('Campaign_Name', inplace=True)
-ad_schedule['Budget'] = ad_schedule.Budget.str.replace('$', '').str.replace(',', '').astype(float)
+ad_schedule['Budget'] = ad_schedule.Budget.str.replace('\$|,','',regex=True).astype(float)
 ad_schedule.drop(columns=['Type', 'When', 'Media', 'Audience File', 'Notes', 'Unnamed: 15', 'Platform'], inplace=True)
 
 #merging facebook_instagram and pinterest (eventually twitter)
@@ -85,11 +94,9 @@ for value in ad_schedule_merge.Objective:
 #filling NaN in clicks with 0.0
 ad_schedule_merge.Clicks.fillna(0.0, inplace=True)
 
-#removing ',' from results and changing to float
-ad_schedule_merge['Results'] = ad_schedule_merge.Results.astype('float')
-
-#change reach to float
-ad_schedule_merge['Reach'] = ad_schedule_merge.Reach.astype('float')
+#changing Results and Reach to float
+for column in ['Results', 'Reach']:
+  ad_schedule_merge[column] = ad_schedule_merge[column].astype('float')
 
 #creating CPM, CPC, CTR column - calculations using reach
 ad_schedule_merge['CPM'] = (ad_schedule_merge['Spend'] / ad_schedule_merge['Reach']) * 1000
