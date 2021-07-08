7a1f1f485ea68ce6da723a36056ee16364c8608c
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-07-08 15:03:40 -0400

    Drops duplicates and converts date cols to dt

--- src/Monisha/social_media_ads/2020/analysis/analysis.py
+++ src/Monisha/social_media_ads/2020/analysis/analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 import toml
 import src.utils as u
+import re
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 config = toml.load(os.path.join(this_dir, 'config.toml'))
@@ -16,4 +17,11 @@ pd.set_option('display.max_columns', None)
 
 #merging social data and google data
 all_data = pd.concat([social_merged, google_merged])
+
+#misc cleaning - drop duplicates, drop unnamed col, change date cols to date time
 all_data.drop_duplicates(subset='Campaign_Name', keep='last', inplace=True)
+all_data.drop(columns='Unnamed: 0', inplace=True)
+
+date_cols = [column for column in all_data.columns if re.match('.*Date', column)]
+for name in date_cols:
+  all_data[name] = pd.to_datetime(all_data[name])
