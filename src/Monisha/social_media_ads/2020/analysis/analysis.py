3e4f2779172474460c868745417e434c2ef71290
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-07-08 16:23:59 -0400

    Breaks up df by objective

--- src/Monisha/social_media_ads/2020/analysis/analysis.py
+++ src/Monisha/social_media_ads/2020/analysis/analysis.py
@@ -25,3 +25,51 @@ all_data.drop(columns='Unnamed: 0', inplace=True)
 date_cols = [column for column in all_data.columns if re.match('.*Date', column)]
 for name in date_cols:
   all_data[name] = pd.to_datetime(all_data[name])
+
+#breaking up by objective
+print(all_data.Objective.value_counts())
+traffic = all_data.loc[all_data.Objective == 'Traffic', :]
+conversions = all_data.loc[all_data.Objective == 'Conversions', :]
+engagement = all_data.loc[all_data.Objective == 'Engagement', :]
+reach = all_data.loc[all_data.Objective == 'Reach', :]
+page_likes = all_data.loc[all_data.Objective == 'Page Likes', :]
+video_views = all_data.loc[all_data.Objective == 'Video Views', :]
+"""
+Presentation notes
+Traffic        200
+Conversions     26
+Engagement      13
+Reach            9
+Page Likes       3
+Video Views      2
+"""
+
+#print(conversions.Audience.value_counts())
+
+#sns.barplot(x='Objective', y='Reach', data=all_data)
+#sns.barplot(x='Objective', y='Clicks', data=all_data)
+#sns.barplot(x='Objective', y='CPM', data=all_data)
+#sns.barplot(x='Result_Type', y='CPM', data=all_data)
+#sns.barplot(x='Objective', y='CPC', data=all_data)
+#sns.barplot(x='Result_Type', y='CPC', data=all_data)
+
+#sns.barplot(x='Objective', y='CTR', data=all_data)
+#sns.barplot(x='Objective', y='Total_Users', data=all_data)
+#sns.barplot(x='Objective', y='Total_Users', data=all_data)
+#sns.barplot(x='Result_Type', y='Total_Users', data=all_data)
+
+#sns.barplot(x='Objective', y='Total_Click_To_Retail', data=all_data)
+#sns.barplot(x='Result_Type', y='Total_Click_To_Retail', data=all_data)
+
+#sns.barplot(x='Objective', y='Cost_Per_User', data=all_data)
+#sns.barplot(x='Objective', y='Cost_Per_Click_To_Retail', data=all_data)
+
+
+#all_data_corr = all_data.corr()
+#sns.heatmap(all_data_corr, annot=True)
+#sns.lmplot(x='Spend', y='Clicks', data=all_data)
+#sns.lmplot(x='Spend', y='Reach', data=all_data)
+#sns.lmplot(x='Spend', y='CPC', data=all_data)
+#sns.pairplot(all_data)
+
+#plt.show()
