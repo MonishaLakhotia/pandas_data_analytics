02dc8509f9b6e944c4e1f920257ae3b4b4063a5a
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-07-08 14:41:15 -0400

    Read files into doc

--- src/Monisha/social_media_ads/2020/analysis/analysis.py
+++ src/Monisha/social_media_ads/2020/analysis/analysis.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+import toml
+import src.utils as u
 
+this_dir = os.path.dirname(os.path.realpath(__file__))
+config = toml.load(os.path.join(this_dir, 'config.toml'))
+u.set_full_paths(config, this_dir)
+
+social_merged = pd.read_csv(config['file_locations']['social_merged'])
+google_merged = pd.read_csv(config['file_locations']['google_merged'])
