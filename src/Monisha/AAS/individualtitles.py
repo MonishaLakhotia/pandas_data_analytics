3560dacbd292c86538ac3fc6af724f91d2df890f
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-10-06 13:25:05 -0400

    Imports requests package

--- src/Monisha/AAS/individualtitles.py
+++ src/Monisha/AAS/individualtitles.py
@@ -5,6 +5,8 @@ import pandas as pd
 import src.utils as u
 import re
 import toml
+import requests
+#make sure to pip install requests again code: python -m pip install requests
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
