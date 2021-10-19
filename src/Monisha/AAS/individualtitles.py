5bfee6c0f6ac3320934ab37feafd64a931a66d34
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-10-19 16:48:06 -0400

    Creates dict of dfs for sheet names

--- src/Monisha/AAS/individualtitles.py
+++ src/Monisha/AAS/individualtitles.py
@@ -81,25 +81,27 @@ reordered = book_data_merge[['ASIN', 'Title', 'Author', 'Pub_Date',
 'Impressions', 'Clicks', 'Orders', 'Spend', 'Sales',
 'CTR',  'CPC', 'ACOS']]
 
-#to groupby title and author (allows NaN) - KEEP THIS AFTER THE REST OF THE CLEANING
-title_author = reordered.groupby(['Title', 'Author'], dropna=False).sum()
 
-#for loop to groupby subgenre, format, author (allows NaN)- KEEP THIS AFTER THE REST OF THE CLEANING
+#for loop to groupby subgenre, format, author (allows NaN)
 d={}
 to_groupby = ['Assumed_Subgenre', 'First_BISAC_Subject', 'Format', 'Author', 'Series_Number']
 for series_group in to_groupby:
   df_name = series_group.lower()
   d[df_name] = pd.DataFrame(reordered.groupby(series_group, dropna=False).sum())
 
-#to group by backlist and front list (allows NaN)- KEEP THIS AFTER THE REST OF THE CLEANING
+#to groupby title and author (allows NaN)
+d['title_author'] = pd.DataFrame(reordered.groupby(['Title', 'Author'], dropna=False).sum())
+
+#to group by backlist and front list (allows NaN)
 six_months = date.today() - relativedelta(months=+6)
 backlist = reordered.loc[reordered.Pub_Date < six_months]
-backlist_asin_merge = backlist.groupby(['ASIN', 'Title', 'Author', 'Pub_Date'], dropna=False).sum()
 frontlist = reordered.loc[reordered.Pub_Date >= six_months]
-frontlist_asin_merge = frontlist.groupby(['ASIN', 'Title', 'Author', 'Pub_Date'], dropna=False).sum()
+d['backlist'] = pd.DataFrame(backlist.groupby(['ASIN', 'Title', 'Author', 'Pub_Date'], dropna=False).sum())
+d['frontlist'] = pd.DataFrame(frontlist.groupby(['ASIN', 'Title', 'Author', 'Pub_Date'], dropna=False).sum())
 
 #sets index for reordered to Title
 reordered.set_index(reordered.Title, inplace=True)
+d['raw'] = reordered
 
 #creates function to fix CTR, CPC, and ACOS when merging and sort by Orders/ACOS - KEEP THIS AFTER THE REST OF THE CLEANING FOR NOW
 def agg_functions(df):
@@ -108,30 +110,23 @@ def agg_functions(df):
   df['ACOS'] = df.Spend / df.Sales
   df.sort_values(['Orders', 'ACOS'], ascending=False, inplace=True)
 
-#for loop with agg_functions
-dataframes = [reordered, title_author, d['assumed_subgenre'],
-d['first_bisac_subject'], d['format'], d['author'], d['series_number'], 
-backlist_asin_merge, frontlist_asin_merge]
-for df in dataframes:
-  agg_functions(df)
+for df in d.keys():
+  agg_functions(d[df])
 
 #to save as multisheet xlsx
 file_location = ExcelWriter(config['file_locations']['output'])
-
-def save_xls(list_dfs, xls_path):
-  with ExcelWriter(xls_path) as writer:
-
-    for n, df in enumerate(list_dfs):
-      df.to_excel(writer,'sheet%s' % n)  
-    writer.save()
-
-save_xls(dataframes,file_location)
-
+for key in d:
+  d[key].to_excel(file_location, key)
+file_location.save()  
 
 """
 TO DO:
 -Figure out next steps - 
   -probably download all and send to self to decide what else is left 
+  -COMMIT these changes before you do the next set of changes
+  -double check all the agg functions to make sure it's correct
+  -will have to deal with inf situation again - see one of the previous codes for fix
+
 NOTE
 the CTR and ACOS are not in % form, need to multiply by 100 and add percent sign
 
