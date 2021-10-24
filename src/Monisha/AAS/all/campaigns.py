f9ad5d6a2ab946d0709150e2d55b9b1c4b79ce3c
Monisha Lakhotia <monishalakhotia@yahoo.com>
2021-10-24 14:19:28 -0400

    Renamed file

--- src/Monisha/AAS/all/campaigns.py
+++ src/Monisha/AAS/all/campaigns.py
@@ -21,31 +21,33 @@ config = toml.load(os.path.join(this_dir, 'config.toml'))
 # prefix partial file paths found in the config.toml with the full path
 u.set_full_paths(config, this_dir)
 
-all_campaigns = pd.read_csv(config['file_locations']['all_campaigns'])
-aas_schedule = pd.read_csv(config['file_locations']['aas_schedule'])
+
+files = sorted(glob(os.path.join(this_dir, config['file_locations']['all_campaigns'])))
+all_campaigns_df: pd.DataFrame = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
+aas_schedule_df: pd.DataFrame = pd.read_csv(config['file_locations']['aas_schedule'])
 individual_titles_output = pd.read_excel(config['file_locations']['individual_titles_output'], sheet_name=None)
 
 #drops columns from both dfs
-all_campaigns.drop(columns=['State', 'Status', 'Campaign bidding strategy',
+all_campaigns_df.drop(columns=['State', 'Status', 'Campaign bidding strategy',
 'Portfolio', 'Budget(USD)', 'Cost type', 'ROAS', 'Viewable impressions', 
 'VCPM(USD)', 'Start date', 'End date'], 
 inplace=True)
-aas_schedule.drop(columns=['Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10'], inplace=True)
+aas_schedule_df.drop(columns=['Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10'], inplace=True)
 
 #adds underscore and deals with caps from both dfs
-all_campaigns.columns = all_campaigns.columns.str.title().str.replace(' ', '_').str.replace('\(Usd\)', '')
-all_campaigns.rename(columns={'Ctr': 'CTR', 'Cpc': 'CPC', 'Acos': 'ACOS'}, inplace=True)
-aas_schedule.columns = aas_schedule.columns.str.replace(' ', '_')
+all_campaigns_df.columns = all_campaigns_df.columns.str.title().str.replace(' ', '_').str.replace('\(Usd\)', '')
+all_campaigns_df.rename(columns={'Ctr': 'CTR', 'Cpc': 'CPC', 'Acos': 'ACOS'}, inplace=True)
+aas_schedule_df.columns = aas_schedule_df.columns.str.replace(' ', '_')
 
 #formatting type and targeting for all_campaigns
-all_campaigns['Type'] = all_campaigns.Type.replace('SP', 'Sponsored Product')
-all_campaigns['Targeting'] = all_campaigns.Targeting.str.title()
+all_campaigns_df['Type'] = all_campaigns_df.Type.replace('SP', 'Sponsored Product')
+all_campaigns_df['Targeting'] = all_campaigns_df.Targeting.str.title()
 
 #merging start and end dates to all_campaigns
-for_merge = aas_schedule.loc[:, ['Campaign_Name', 'Start_Date', 'End_Date']]
+for_merge = aas_schedule_df.loc[:, ['Campaign_Name', 'Start_Date', 'End_Date']]
 for_merge.drop(for_merge.loc[for_merge.Campaign_Name.isna(), :].index, inplace=True)
 for_merge.rename(columns={'Campaign_Name': 'Campaigns'}, inplace=True)
-merged_campaigns = pd.merge(all_campaigns, for_merge, on='Campaigns')
+merged_campaigns = pd.merge(all_campaigns_df, for_merge, on='Campaigns')
 
 #sorting merged_campaigns
 merged_campaigns.sort_values(['Orders', 'ACOS'], ascending=False, inplace=True)
@@ -55,12 +57,12 @@ merged_campaigns.loc['Total'] = merged_campaigns.sum(numeric_only=True, axis=0)
 merged_campaigns.loc['Total', 'Campaigns'] = 'Total'
 
 #adding total row to aas schedule Spend col
-aas_schedule['Budget'] = aas_schedule.Budget.str.replace('\$|,', '')
-aas_schedule['Budget'] = aas_schedule.Budget.str.split('.').str[0]
-aas_schedule.dropna(how='all', inplace=True)
-aas_schedule['Budget'] = aas_schedule.Budget.astype(int)
-aas_schedule.loc['Total', 'Budget'] = aas_schedule.Budget.sum()
-aas_schedule.loc['Total', 'Campaign'] = 'Total'
+aas_schedule_df['Budget'] = aas_schedule_df.Budget.str.replace('\$|,', '')
+aas_schedule_df['Budget'] = aas_schedule_df.Budget.str.split('.').str[0]
+aas_schedule_df.dropna(how='all', inplace=True)
+aas_schedule_df['Budget'] = aas_schedule_df.Budget.astype(int)
+aas_schedule_df.loc['Total', 'Budget'] = aas_schedule_df.Budget.sum()
+aas_schedule_df.loc['Total', 'Campaign'] = 'Total'
 
 #calling agg_functions and meeting_format on merged_campaigns
 agg_functions(merged_campaigns)
@@ -72,7 +74,7 @@ reordered = merged_campaigns[['Campaigns', 'Start_Date', 'End_Date', 'Type', 'Ta
 'Impressions', 'Clicks', 'Orders', 'Spend', 'Sales', 'CTR', 'CPC', 'ACOS']]
 
 #add to the individual_titles_output dictionary
-individual_titles_output['aas_schedule'] = aas_schedule
+individual_titles_output['aas_schedule'] = aas_schedule_df
 individual_titles_output['all_campaigns'] = reordered
 
 #reset the index of all sheets
